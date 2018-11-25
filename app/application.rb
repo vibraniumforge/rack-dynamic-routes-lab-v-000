28d05d1227032d772161f1343fc1f0225fc68152
Greg Morgan <gregm9999@gmail.com>
2018-11-25 22:20:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 require "pry"
 class Application
 
+  @@items=[]
+
   def call (env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    @@items=[]
+
 
     if req.path.match(/items/)
       search_term = req.path.split("/items/").last
