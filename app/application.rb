3e7fdc2a9bd9f1c36eaa3d3574abd9fe2784a412
Greg Morgan <gregm9999@gmail.com>
2018-11-25 22:14:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
 
     if req.path.match(/items/)
       search_term = req.path.split("/items").last
-      if @@items.contain?(search_item)
+      if @@items.contain?(search_term)
         resp.write "#{item.price}"
       else
         resp.write "Item not found"
