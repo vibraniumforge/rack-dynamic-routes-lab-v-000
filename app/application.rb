66eb9c1f13124373521b2ab473190d794c60fd8c
Greg Morgan <gregm9999@gmail.com>
2018-11-25 22:16:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 class Application
 
   def call (env)
@@ -10,6 +10,7 @@ class Application
     if req.path.match(/items/)
       search_term = req.path.split("/items").last
       item = @@items.find {|i| i.name=search_term}
+      binding.pry
       if item.nil?
         resp.write "Item not found"
         resp.status =400
