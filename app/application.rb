d5489fa99568f3951f5bef9993734c5c7e2aa2f9
Greg Morgan <gregm9999@gmail.com>
2018-11-25 22:04:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call (env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    @@items=[]
     if req.path=="items/item/"
       if @@items.contain?(item)
         resp.write "#{item.price}"
