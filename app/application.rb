d3627a1217a1bf2b62971d15633020eb61ca890d
Greg Morgan <gregm9999@gmail.com>
2018-11-25 22:03:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call (env)
     resp = Rack::Response.new
-    req = Rack::Request.new
+    req = Rack::Request.new(env)
 
     if req.path=="items/item/"
       if @@items.contain?(item)
